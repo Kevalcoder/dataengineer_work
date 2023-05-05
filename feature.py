552a4a5ec17b9aabf199cfc09d1a618024db875a
Keval <79027056+Kevalcoder@users.noreply.github.com>
2023-05-05 13:02:51 -0400

    Update feature.py

--- feature.py
+++ feature.py
@@ -1,6 +1,6 @@
 import pandas as pd
 # Load the dataset from the previous problem
-df = pd.read_csv('data.csv')
+df = pd.read_parquet('data.parquet')
 
 # Sort the dataframe by Date
 df = df.sort_values('Date')
@@ -12,4 +12,4 @@ df['vol_moving_avg'] = df.groupby('Symbol')['Volume'].rolling(window=30).mean().
 df['adj_close_rolling_med'] = df.groupby('Symbol')['Adj Close'].rolling(window=30).median().reset_index(level=0, drop=True)
 
 # Save the resulting dataset to a new Parquet file
-df.to_csv('new_dataset.csv')
+df.to_parquet('new_dataset.parquet')
