ba9263e2b7613f683cec23c48e005a6e954b1cef
Keval <79027056+Kevalcoder@users.noreply.github.com>
2023-05-05 23:25:18 -0400

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -5,21 +5,16 @@ class TestMovingAverage(unittest.TestCase):
     
     def test_vol_moving_avg(self):
         # Create a sample DataFrame
-        data = {
-            'Symbol': ['ABC', 'ABC', 'DEF', 'DEF'],
-            'Date': ['2021-01-01', '2021-01-02', '2021-01-01', '2021-01-02'],
-            'Volume': [100, 200, 50, 75]
-        }
-        df = pd.DataFrame(data)
-        
-        # Calculate the expected result
-        expected = [pd.NA, pd.NA, pd.NA, 108.33]
-        
-        # Calculate the moving average
-        df['vol_moving_avg'] = df.groupby('Symbol')['Volume'].rolling(window=3).mean().reset_index(0, drop=True)
-        
-        # Check that the result is correct
-        self.assertEqual(list(df['vol_moving_avg']), expected)
+        self.df = pd.DataFrame({
+            'Symbol': ['AAPL', 'AAPL', 'AAPL', 'AAPL', 'AAPL', 'AAPL', 'AAPL', 'AAPL'],
+            'Date': ['2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04', '2022-01-05', '2022-01-06', '2022-01-07', '2022-01-08'],
+            'Adj Close': [100.0, 110.0, 120.0, 130.0, 140.0, 150.0, 160.0, 170.0]
+        })
+        self.expected = pd.Series([pd.NaT, pd.NaT, pd.NaT, pd.NaT, pd.NaT, pd.NaT, pd.NaT, 125.0])
+
+    def test_rolling_median(self):
+        result = self.df.groupby('Symbol')['Adj Close'].rolling(window=3).median().reset_index(0, drop=True)
+        self.assertTrue(self.expected.equals(result))
 
 if __name__ == '__main__':
     unittest.main()
